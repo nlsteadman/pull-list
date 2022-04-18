37d7957544f2c925d4b7ec32f3960597bbd8990d
Nicole Steadman <nsteadman@gmail.com>
2022-04-17 19:40:01 -0700

    seeded database using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+50.times do
+    Comic.create(
+        name: Faker::DcComics.title,
+        publisher: "DC",
+        description: Faker::Lorem.sentence
+    )
+end
+
+50.times do
+    User.create(
+        name: Faker::Name.name,
+        address: Faker::Address.street_address,
+        phone_number: Faker::PhoneNumber.phone_number,
+        email: "email@email.com"
+    )
+end
+
+50.times do
+    UserComic.create(
+        quantity: rand(0..5)
+    )
+end
 
 puts "✅ Done seeding!"
