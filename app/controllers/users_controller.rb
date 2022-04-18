cad4b3b11b777e9797f03af1ca66b1c147c54ff5
Nicole Steadman <nsteadman@gmail.com>
2022-04-17 19:44:40 -0700

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
+    get "/users" do
+        user = User.all
+        user.to_json
+    end
 
+    post "/users" do
+        user = User.new(params[:user])
+        if user.save
+            user.to_json
+        else
+            {errors: user.errors.full_messages}.to_json
+        end
+    end
 end
