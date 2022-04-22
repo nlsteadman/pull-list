2e9ffd6d80856e9d759741bc9436198714993551
Nicole Steadman <nsteadman@gmail.com>
2022-04-22 11:44:11 -0700

    added errors

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,5 +2,5 @@ class Comic < ActiveRecord::Base
     has_many :user_comics
     has_many :users, through: :user_comics
 
-    validates :name, presence: true, uniqueness: true
+    validates_presence_of :name, :description, :publisher, uniqueness: true
 end
