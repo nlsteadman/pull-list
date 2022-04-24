ead43939c4b61308bebc3186978afaaa3f5d002e
Nicole Steadman <nsteadman@gmail.com>
2022-04-24 10:50:26 -0700

    updated comic.rb validations

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,5 +2,5 @@ class Comic < ActiveRecord::Base
     has_many :user_comics
     has_many :users, through: :user_comics
 
-    validates_presence_of :name, :description, :publisher, uniqueness: true
+    validates_presence_of :name, :description, :publisher
 end
