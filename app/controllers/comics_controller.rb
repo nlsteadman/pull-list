aef09ce835b98747d08a60c9c8ee3865c30ed07f
Nicole Steadman <nsteadman@gmail.com>
2022-04-24 16:39:34 -0700

    added delete function

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,23 +1,23 @@
 class ComicsController < ApplicationController
     get "/comics" do
-        comic = Comic.all
-        comic.to_json
+        @comic = Comic.all
+        @comic.to_json
     end
 
     post "/comics" do
-        comic = Comic.new(params[:comic])
-        if comic.save
-            comic.to_json
+        @comic = Comic.new(params[:comic])
+        if @comic.save
+            @comic.to_json
         else
-            {errors: comic.errors.full_messages}.to_json
+            {errors: @comic.errors.full_messages}.to_json
         end
     end
 
     delete '/comics/:id' do
         find_comic
-        if comic
-            comic.destroy
-            comic.to_json
+        if @comic
+            @comic.destroy
+            @comic.to_json
         else
             { errors: ["Comic doesn't exist"]}.to_json
         end
@@ -25,7 +25,7 @@ class ComicsController < ApplicationController
 
     private
         def find_comic
-            comic = Comic.find_by_id(params[:id])
+            @comic = Comic.find_by_id(params[:id])
         end
     
 end
