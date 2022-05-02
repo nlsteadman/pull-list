0d19afe81e34b32d0637864abffb4a900d9adb6e
Nicole Steadman <nsteadman@gmail.com>
2022-05-02 11:50:11 -0700

    added methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
+    get "/users" do
+        @user = User.all
+        @user.to_json
+    end
+
     post "/users" do
-        user = User.new(params[:user])
-        if user.save
-            user.to_json
+        @user = User.new(params[:user])
+        if @user.save
+            @user.to_json
         else
-            {errors: user.errors.full_messages}.to_json
+            {errors: @user.errors.full_messages}.to_json
         end
     end
+
+    delete '/users/:id' do
+        find_user
+        if @user
+            @user.destroy
+            @user.to_json
+        else
+            { errors: ["User doesn't exist"]}.to_json
+        end
+    end
+
+    private
+        def find_user
+            @user = User.find_by_id(params[:id])
+        end
 end
