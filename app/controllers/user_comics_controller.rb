7f4d30ef3401fff7836f3f3974e6dc3dab3a1000
Nicole Steadman <nsteadman@gmail.com>
2022-05-06 19:49:14 -0700

    updated user comics controller

--- app/controllers/user_comics_controller.rb
+++ app/controllers/user_comics_controller.rb
@@ -1,7 +1,26 @@
 class UserComicsController < ApplicationController
-    get "/user_comics" do
-        @user_comics = UserComic.all
-        @user_comics.to_json(include: { users: { include: :comics } } )
+    post "/user_comics" do
+        @user_comics = UserComic.new(params[:user_comic])
+        if @user_comic.save
+            @user_comic.to_json
+        else
+            {errors: @user_comic.errors.full_messages}.to_json
+        end
     end
+
+    delete '/user_comics/:id' do
+        find_user_comic
+        if @user_comic
+            @user_comic.destroy
+            @user_comic.to_json
+        else
+            {errors: @user_comic.errors.full_messages}.to_json
+        end
+    end
+
+    private
+        def find_user_comic
+            @user_comic = UserComic.find_by_id(params[:id])
+        end
 
 end
